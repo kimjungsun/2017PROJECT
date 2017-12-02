27d28e77dc954a47a1d6f09e2ee124b671add1e8
Mac <K_JS@Macui-MacBook-Pro.local>
2017-12-02 18:53:38 +0900

    first commit

---
+++ temp.c
@@ -0,0 +1,182 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "datatype.h"
+#include "linkedlist.h"
+mp Mhead;
+dp Dhead;
+ap Ahead;
+ap2 ahead;
+tp thead1;
+tp thead2;
+char *Menu;
+
+
+void search(char *U,FILE *fp,FILE *fp2,FILE *fp3){
+    int i=0;
+	char *option = malloc(sizeof(char)*5);
+	option = NULL;
+	option = strtok(U," ");
+	FILE **fpa = malloc(sizeof(FILE *)*3);
+	if(!strcmp(option,"-m")) fpa[0] = fp;
+	else if(!strcmp(option,"-d")) {fpa[0] = fp2; 
+	option = strtok(NULL,"\0");}
+	else if(!strcmp(option,"-a")) {fpa[0] = fp3; 
+	option = strtok(NULL,"\0");}
+	else if((!strcmp(option,"-md"))||(!strcmp(option,"-dm"))) {fpa[0] = fp;fpa[1]=fp2;
+	option = strtok(NULL,"\0");}
+	else if((!strcmp(option,"-ma"))||(!strcmp(option,"-am"))) {fpa[0] = fp; fpa[1]=fp3;
+	option = strtok(NULL,"\0");}
+	else if((!strcmp(option,"-da"))||(!strcmp(option,"-ad"))) {fpa[0] = fp2;fpa[1]=fp3;
+	option = strtok(NULL,"\0");}
+     else { fpa[0] = fp; fpa[1] = fp2; fpa[2]=fp3;}      
+ 
+	fseek(fp,0,SEEK_SET);
+	fseek(fp2,0,SEEK_SET);
+	fseek(fp3,0,SEEK_SET);
+	char *st = malloc(sizeof(char)*100);
+    while(*fpa != NULL){
+	while(fgets(st,100,*fpa)!=NULL){
+		if(strstr(st,option)){i++; printf("%s\n",st);}
+	
+	}
+   fpa = fpa+1;
+	}
+if(!i) printf("There is no '%s' in file\n",option);
+
+	return ;
+}
+
+void print(char *U){
+	int i,num;
+	char *name = malloc(sizeof(void)*10);
+	void **list = malloc(sizeof(void *)*4);
+    list[0] = Mhead;
+	list[1] = Dhead;
+	list[2] = Ahead;
+
+	char *option = malloc(sizeof(char)*5);
+	option = strtok(U," ");
+	if(!strcmp(option,"m")) {i=0; name = "M";}
+	else if(!strcmp(option,"d")) {	i=1; name = "D"; }
+	else if(!strcmp(option,"a")) {i=2; name = "A";}
+	num = atoi(strtok(NULL," "));
+    void *p = gotoNode(num,list[i],name);
+	
+	if(i==0){
+        mp m = p;
+	printf("%d %s %s %d %d\n",m->num,m->title,m->genre,m->year,m->time);
+	if(m->director->directorP == NULL) printf("%s\n",m->director->director);
+	else printf("%s(%s)\n",m->director->director,m->director->directorP->birth);
+	if(m->actor->next->actorP == NULL) printf("%s\n",m->actor->next->actor);
+	else printf("%s(%s)\n",m->actor->next->actor,m->actor->next->actorP->birth);
+}
+    else if(i==1){
+		dp m = p;
+	printf("%d / %s / %c / %s\n",m->num,m->director,m->sex,m->birth);
+	if(m->title->next->movieP == NULL) printf("%s\n",m->title->next->title);
+	else printf("%s(%d,%dmin)\n",m->title->next->title,m->title->next->movieP->year,m->title->next->movieP->time);
+	}	
+    else if(i==2){
+		ap m = p;
+	printf("%d / %s / %c / %s\n",m->num,m->actor,m->sex,m->birth);
+	if(m->title->next->movieP == NULL) printf("%s\n",m->title->next->title);
+	else printf("%s(%d,%dmin)\n",m->title->next->title,m->title->next->movieP->year,m->title->next->movieP->time);
+	
+	}
+
+}
+void add(char *U){
+	char *option = strtok(U," ");
+    if(
+	
+	
+	
+	
+	
+	
+	void delete(char *U){
+	char *option = strtok(U," ");
+	int num = atoi(strtok(NULL," "));
+	if(!strcmp(option,"m")) {
+		mp cur = gotoNode(num,Mhead,"M");
+		if(cur == NULL) printf("No such record\n");
+		else deleteNode(Mhead,cur,"M");
+	}
+	else if(!strcmp(option,"d")) {
+		dp cur = gotoNode(num,Dhead,"D");
+		if(cur == NULL) printf("No such record\n");
+		else deleteNode(Dhead,cur,"D");
+	}
+	if(!strcmp(option,"a")) {
+	    ap cur = gotoNode(num,Ahead,"A");
+		if(cur == NULL) printf("No such record\n");
+		else deleteNode(Ahead,cur,"A");
+	}
+
+}
+void end(){
+// 파일 저장 및 업데이트 관련 내용 작성 !! 
+
+}
+char *menu(){
+    printf("(movie)");
+	char *U = malloc(sizeof(char)*50);
+	scanf("%[^\n]s",U);
+	getchar();
+	return U;
+}
+
+
+int main(){
+	FILE *fp,*fp2,*fp3;
+	fp = fopen("movielog.txt","r");
+	fp2 = fopen("directorlog.txt","r");
+	fp3 = fopen("actorlog.txt","r");
+	/* 정보 링크드 리스트 생성 */
+
+	init_list(&Mhead);
+	movie_info(&Mhead,fp,fp2);
+
+	init_list2(&Dhead);
+	director_info(&Dhead,fp2);
+	
+	init_list3(&Ahead);
+	actor_info(&Ahead,fp3);
+	
+	linkdirector(&Mhead,&Dhead);
+	linktitle(&Mhead,Dhead,"Dhead");
+	linktitle(&Mhead,Ahead,"Ahead");
+	linkactor(&Mhead,&Ahead);
+
+	/* 메뉴 프롬프트 */
+    int i;
+	char *Menu = malloc(sizeof(char)*50);
+	char *Menu2 = malloc(sizeof(char)*50);
+	char *Menu3 = malloc(sizeof(char)*50);
+	printf("<input :search/print/add/update/delete/sort/save/end>\n");
+	while(1){
+		
+		Menu = menu();
+		Menu2 = strtok(Menu," ");
+        Menu3 = strtok(NULL,"\0");
+		if(!strcmp(Menu2,"search"))search(Menu3,fp,fp2,fp3);
+		else if(!strcmp(Menu,"print")) print(Menu3);
+		else if(!strcmp(Menu,"add")) add(Menu3);
+		else if(!strcmp(Menu,"update")); 
+		else if(!strcmp(Menu,"delete")) delete(Menu3);
+		else if(!strcmp(Menu,"sort")) ;
+		else if(!strcmp(Menu,"save")) ;
+		else if(!strcmp(Menu,"end")){end(); break; }
+		else printf(" 메뉴명을 다시 입력하세요 \n");
+	}
+
+	fclose(fp);
+	fclose(fp2);
+	fclose(fp3);
+	return 0;
+}
+
+
+
+
