309e1951be07ccd53961c782e50b1db254a68b28
Mac <K_JS@Macui-MacBook-Pro.local>
2017-12-08 19:36:15 +0900

    today

--- temp.c
+++ temp.c
@@ -3,173 +3,343 @@
 #include<string.h>
 #include "datatype.h"
 #include "linkedlist.h"
-mp Mhead;
-dp Dhead;
-ap Ahead;
-ap2 ahead;
-tp thead1;
-tp thead2;
-char *Menu;
-int numS;
-
-void search(char *U,FILE *fp,FILE *fp2,FILE *fp3){
-    int i=0;
-	char *option = malloc(sizeof(char)*5);
-	option = NULL;
-	option = strtok(U," ");
-	FILE **fpa = malloc(sizeof(FILE *)*3);
-	if(!strcmp(option,"-m")) fpa[0] = fp;
-	else if(!strcmp(option,"-d")) {fpa[0] = fp2; 
-	option = strtok(NULL,"\0");}
-	else if(!strcmp(option,"-a")) {fpa[0] = fp3; 
-	option = strtok(NULL,"\0");}
-	else if((!strcmp(option,"-md"))||(!strcmp(option,"-dm"))) {fpa[0] = fp;fpa[1]=fp2;
-	option = strtok(NULL,"\0");}
-	else if((!strcmp(option,"-ma"))||(!strcmp(option,"-am"))) {fpa[0] = fp; fpa[1]=fp3;
-	option = strtok(NULL,"\0");}
-	else if((!strcmp(option,"-da"))||(!strcmp(option,"-ad"))) {fpa[0] = fp2;fpa[1]=fp3;
-	option = strtok(NULL,"\0");}
-     else { fpa[0] = fp; fpa[1] = fp2; fpa[2]=fp3;}      
- 
-	fseek(fp,0,SEEK_SET);
-	fseek(fp2,0,SEEK_SET);
-	fseek(fp3,0,SEEK_SET);
-	char *st = malloc(sizeof(char)*100);
-    while(*fpa != NULL){
-	while(fgets(st,100,*fpa)!=NULL){
-		if(strstr(st,option)){i++; printf("%s\n",st);}
-	
-	}
-   fpa = fpa+1;
-	}
-if(!i) printf("There is no '%s' in file\n",option);
-
-	return ;
-}
-
-void print(char *U){
-printf("%s\n",U);
-	printf("\n");
-	int i,num;
-	char *name = malloc(sizeof(void)*10);
-	void **list = malloc(sizeof(void *)*4);
-    list[0] = Mhead;
-	list[1] = Dhead;
-	list[2] = Ahead;
-
-	char *option = malloc(sizeof(char)*5);
-	option = strtok(U," ");
-	if(!strcmp(option,"m")) {i=0; name = "M";}
-	else if(!strcmp(option,"d")) {	i=1; name = "D"; }
-	else if(!strcmp(option,"a")) {i=2; name = "A";}
-	num = atoi(strtok(NULL," "));
-	void *p = gotoNode(num,list[i],name);
-	
-	if(i==0){
-        mp m = p;
-		actors *a = m->actor;
-	printf("%d / %s / %s / %d / %d\n",m->num,m->title,m->genre,m->year,m->time);
-
-	
-	if(m->director->directorP == NULL) printf("%s\n",m->director->director);
-	else printf("%s(%s)\n",m->director->director,m->director->directorP->birth);
-    
-	while(a->next != NULL){
-	if(a->next->actorP == NULL) printf("%s\n",a->next->actor);
-	else printf("%s(%s)\n",a->next->actor,a->next->actorP->birth);
-    a = a->next;
-	}
-}
-    else if(i==1){
-		dp m = p;
-		titles *a = m -> title;
-	printf("%d / %s / %c / %s\n",m->num,m->director,m->sex,m->birth);
-	
-	while(a->next != NULL){
-	if(a->next->movieP == NULL) printf("%s\n",a->next->title);
-	else printf("%s(%d,%dmin)\n",a->next->title,a->next->movieP->year,a->next->movieP->time);
-    a = a->next;
-	}	
-	}
-    else if(i==2){
-		ap m = p;
-		titles *a = m -> title;
-	printf("%d / %s / %c / %s\n",m->num,m->actor,m->sex,m->birth);
-	
-	while(a->next != NULL){
-	if(a->next->movieP == NULL) printf("%s\n",a->next->title);
-	else printf("%s(%d,%dmin)\n",a->next->title,a->next->movieP->year,a->next->movieP->time);
-	a = a->next;
-	}
-	}
-
-}
-void add(char *U){
-	char *option = strtok(U," ");
-	if(!strcmp(option,"m")) {
-        mp cur= malloc(sizeof(movie));
-        cur->title = malloc(sizeof(char)*30);
-        cur->genre = malloc(sizeof(char)*30);
-		cur->director = malloc(sizeof(directorlist));
-        cur->director->director = malloc(sizeof(char)*30);
-	    ap2 temp = malloc(sizeof(actors));
-		ap2 ahead = temp;
+	mp Mhead;
+	dp Dhead;
+	ap Ahead;
+	ap2 ahead;
+	tp thead1;
+	tp thead2;
+	char *Menu;
+	int numS;
+	void search(char *U){
+		FILE *f = fopen("movielist.txt","r");
+		FILE *f2 = fopen("directorlist.txt","r");
+		FILE *f3 = fopen("actorlist.txt","r");
+		int i=0;
+		int check = 0;
+		char *option = malloc(sizeof(char)*5);
+		option = NULL;
+		option = strtok(U," ");
+		FILE **fpa = malloc(sizeof(FILE *)*3);
+		if(!strcmp(option,"-m")){ fpa[0] = f;}
+		else if(!strcmp(option,"-d")) {fpa[0] = f2; }
+		else if(!strcmp(option,"-a")) {fpa[0] = f3; }
+		else if((!strcmp(option,"-md"))||(!strcmp(option,"-dm"))) {fpa[0] = f;fpa[1]=f2;}
+		else if((!strcmp(option,"-ma"))||(!strcmp(option,"-am"))) {fpa[0] = f; fpa[1]=f3;}
+		else if((!strcmp(option,"-da"))||(!strcmp(option,"-ad"))) {fpa[0] = f2;fpa[1]=f3;}
+		 else { check = 1;fpa[0] = f; fpa[1] = f2; fpa[2]=f3;}      
+	 
+		fseek(f,0,SEEK_SET);
+		fseek(f2,0,SEEK_SET);
+		fseek(f3,0,SEEK_SET);
+		char *st = malloc(sizeof(char)*100);
+		char *st1 = malloc(sizeof(char)*100);
+		if(check) st1 = option;
+		else st1 = strtok(NULL,"\0");
+		while(*fpa != NULL){
+		while(fgets(st,100,*fpa)!=NULL){
+			if(strstr(st,st1)){i++; printf("%s\n",st);}
 		
-		cur->num = 13;
-		printf("TITLE > ");
-		scanf("%s",cur->title);
-		printf("GENRE > ");
-		scanf("%s",cur->genre);
-	    printf("DIRECTOR > ");
-        scanf("%s",cur->director->director);
-        cur->director->directorP= NULL;    	
-        printf("YEAR > ");
-		scanf("%d",&(cur->year));
-		printf("TIME > ");
-		scanf("%d",&(cur->time));
-         	getchar();
-		while(1){
-			printf("ACTORS >");
-			temp ->next = malloc(sizeof(actors));
-			temp->next->actor = malloc(sizeof(char)*30);
-			scanf("%[^\n]s",temp -> next->actor);
-	        getchar();
-			temp -> next -> actorP = NULL;
-			temp -> next ->next = NULL;
-		   if(!strcmp(temp->next->actor,"end")) {temp->next=NULL;break;}
-		  temp = temp->next;
-		} 
-		
-		cur->actor = ahead; 
-		cur->next = NULL;
-		addNode(&Mhead,cur);
-
+		}
+	   fpa = fpa+1;
+		}
+	if(!i) printf("There is no %s  in file\n",st1);
+    fclose(f);
+	fclose(f2);
+	fclose(f3);
+		return ;
 	}
 
+	void print(char *U){
+	printf("%s\n",U);
+		printf("\n");
+		int i,num;
+		char *name = malloc(sizeof(void)*10);
+		void **list = malloc(sizeof(void *)*4);
+		list[0] = Mhead;
+		list[1] = Dhead;
+		list[2] = Ahead;
+
+		char *option = malloc(sizeof(char)*5);
+		option = strtok(U," ");
+		if(!strcmp(option,"m")) {i=0; name = "M";}
+		else if(!strcmp(option,"d")) {	i=1; name = "D"; }
+		else if(!strcmp(option,"a")) {i=2; name = "A";}
+		num = atoi(strtok(NULL," "));
+		void *p = gotoNode(num,list[i],name);
+		if(i==0){
+			mp m = p;
+           if(!m) {printf("No serialNo.%d's info in the directory\n",num); return;}
+			actors *a = m->actor;
+		printf("%d / %s / %s / %d / %d\n",m->num,m->title,m->genre,m->year,m->time);
+
+		
+		if(m->director->directorP == NULL) printf("%s\n",m->director->director);
+		else printf("%s(%s)\n",m->director->director,m->director->directorP->birth);
+		
+		while(a->next != NULL){
+		if(a->next->actorP == NULL) printf("%s\n",a->next->actor);
+		else printf("%s(%s)\n",a->next->actor,a->next->actorP->birth);
+		a = a->next;
+		}
+	}
+		else if(i==1){
+			dp m = p;
+           if(!m) {printf("No serialNo.%d's info in the directory\n",num); return;}
+			titles *a = m -> title;
+		printf("%d / %s / %c / %s\n",m->num,m->director,m->sex,m->birth);
+		
+		while(a->next != NULL){
+		if(a->next->movieP == NULL) printf("%s\n",a->next->title);
+		else printf("%s(%d,%dmin)\n",a->next->title,a->next->movieP->year,a->next->movieP->time);
+		a = a->next;
+		}	
+		}
+		else if(i==2){
+			ap m = p;
+           if(!m) {printf("No serialNo.%d's info in the directory\n",num); return;}
+			titles *a = m -> title;
+		printf("%d / %s / %c / %s\n",m->num,m->actor,m->sex,m->birth);
+		
+		while(a->next != NULL){
+		if(a->next->movieP == NULL) printf("%s\n",a->next->title);
+		else printf("%s(%d,%dmin)\n",a->next->title,a->next->movieP->year,a->next->movieP->time);
+		a = a->next;
+		}
+		}
+		else {printf("No option '%s'. Try again.\n",option); return ;}
+
+	}
+	void add(char *U){
+		char *option = strtok(U," ");
+		if(!strcmp(option,"m")) {
+		FILE *f = fopen("movielog.txt","a");
+		FILE *f2 = fopen("movielist.txt","a");	
+		    mp cur= malloc(sizeof(movie));
+			cur->title = malloc(sizeof(char)*30);
+			cur->genre = malloc(sizeof(char)*30);
+			cur->director = malloc(sizeof(directorlist));
+			cur->director->director = malloc(sizeof(char)*30);
+			ap2 temp = malloc(sizeof(actors));
+			ap2 ahead = temp;
+			char *str = malloc(sizeof(char)*110);
+			char *str2 = malloc(sizeof(char)*110);
+			
+			cur->num = (curNum1)++;
+			printf("TITLE > ");
+			scanf("%[^\n]s",cur->title);
+			getchar();
+			printf("GENRE > ");
+			scanf("%s",cur->genre);
+			printf("DIRECTOR > ");
+			getchar();
+			scanf("%[^\n]s",cur->director->director);
+			getchar();
+			cur->director->directorP= NULL;    	
+			printf("YEAR > ");
+			scanf("%d",&(cur->year));
+			printf("TIME > ");
+			scanf("%d",&(cur->time));
+				getchar();
+			sprintf(str,"add:%d:%s:%s:%s:%d:%d:",cur->num,cur->title,cur->genre,cur->director->director,cur->year,cur->time);
+				fputs(str,f);
+			sprintf(str2,"%d:%s:%s:%s:%d:%d:",cur->num,cur->title,cur->genre,cur->director->director,cur->year,cur->time);
+				fputs(str2,f2);
+		int i=0;
+			while(1){
+				printf("ACTORS >");
+				temp ->next = malloc(sizeof(actors));
+				temp->next->actor = malloc(sizeof(char)*30);
+				scanf("%[^\n]s",temp -> next->actor);
+				printf("%s\n",temp->next->actor);
+				getchar();
+				temp -> next -> actorP = NULL;
+				temp -> next ->next = NULL;
+			   if(!strcmp(temp->next->actor,"end")) {
+				   fputc('\n',f);
+				   fputc('\n',f2);
+				   temp->next=NULL;break;}
+			  if(i++) fputs(",",f);
+			  fputs(temp->next->actor,f2);
+			   temp = temp->next;
+			} 
+			
+			cur->actor = ahead; 
+			cur->next = NULL;
+			addNode(&Mhead,cur);
+	fclose(f);
+	fclose(f2);
+		}
+		else if(!strcmp(option,"d")) {
+		FILE *f = fopen("directorlog.txt","a");
+		FILE *f2 = fopen("directorlist.txt","a");
+			dp cur= malloc(sizeof(director));
+			cur->director = malloc(sizeof(char)*30);
+			cur->birth = malloc(sizeof(char)*30);
+			tp temp = malloc(sizeof(titles));
+			tp ahead = temp;
+			cur->num = curNum2++;
+			char *str = malloc(sizeof(char)*100);
+			char *str2 = malloc(sizeof(char)*100);
+				int i=0;
+		
+			printf("Director > ");
+			scanf("%[^\n]s",cur->director);
+				getchar();
+			printf("Sex > ");
+			scanf(" %c",&cur->sex);
+			printf("Birth > ");
+			scanf("%s",cur->birth);
+				getchar();
+		
+			sprintf(str,"add:%d:%s:%c:%s:",cur->num,cur->director,cur->sex,cur->birth);
+				fputs(str,f);
+			sprintf(str2,"%d:%s:%c:%s:",cur->num,cur->director,cur->sex,cur->birth);
+				fputs(str2,f2);
+			while(1){
+				printf("Titles >");
+				temp ->next = malloc(sizeof(titles));
+				temp->next->title = malloc(sizeof(char)*30);
+				scanf("%[^\n]s",temp -> next->title);
+				getchar();
+				temp -> next -> movieP = NULL;
+				temp -> next ->next = NULL;
+			   if(!strcmp(temp->next->title,"end")) {
+				   fputc('\n',f);
+				   temp->next=NULL;break;}
+				if(i++) fputs(",",f);
+			   fputs(temp->next->title,f2);
+			  temp = temp->next;
+			} 
+			
+			cur->title = ahead; 
+			cur->next = NULL;
+			addNode2(&Dhead,cur);
+	fclose(f);
+	fclose(f2);
+	}
+
+		else if(!strcmp(option,"a")) {
+		FILE *f = fopen("actorlog.txt","a");
+		FILE *f2 = fopen("actorlist.txt","a");
+			ap cur= malloc(sizeof(actor));
+			cur->actor = malloc(sizeof(char)*30);
+			cur->birth = malloc(sizeof(char)*30);
+			tp temp = malloc(sizeof(titles));
+			tp thead = temp;
+			cur->num = curNum2++;
+			char *str = malloc(sizeof(char)*100);
+			char *str2 = malloc(sizeof(char)*100);
+				int i=0;
+		
+			printf("Actor > ");
+			scanf("%[^\n]s",cur->actor);
+				getchar();
+			printf("Sex > ");
+			scanf(" %c",&cur->sex);
+			printf("Birth > ");
+			scanf("%s",cur->birth);
+				getchar();
+		
+			sprintf(str,"add:%d:%s:%c:%s:",cur->num,cur->actor,cur->sex,cur->birth);
+				fputs(str,f);
+			sprintf(str2,"%d:%s:%c:%s:",cur->num,cur->actor,cur->sex,cur->birth);
+				fputs(str2,f2);
+			while(1){
+				printf("Titles >");
+				temp ->next = malloc(sizeof(titles));
+				temp->next->title = malloc(sizeof(char)*30);
+				scanf("%[^\n]s",temp -> next->title);
+				getchar();
+				temp -> next -> movieP = NULL;
+				temp -> next ->next = NULL;
+			   if(!strcmp(temp->next->title,"end")) {
+				   fputc('\n',f);
+				   temp->next=NULL;break;}
+				if(i++) fputs(",",f);
+			   fputs(temp->next->title,f2);
+			  temp = temp->next;
+			} 
+			
+			cur->title = thead; 
+			cur->next = NULL;
+			addNode3(&Ahead,cur);
+	fclose(f);
+	fclose(f2);
+	}
 }
 
 void delete(char *U){
 	char *option = strtok(U," ");
 	int num = atoi(strtok(NULL," "));
+	char *str = malloc(sizeof(char)*40);
 	if(!strcmp(option,"m")) {
 		mp cur = gotoNode(num,Mhead,"M");
-		if(cur == NULL) printf("No such record\n");
-		else deleteNode(Mhead,num,"M");
+           if(cur==NULL) {printf("No serialNo.%d's info in the directory\n",num);return;}		
+		else {
+		FILE *f = fopen("movielog.txt","a");
+	sprintf(str,"delete:%d:%s:%s:%s:%d:%d:",cur->num,cur->title,cur->genre,cur->director->director,cur->year,cur->time);
+       fputs(str,f);
+		int i=0;
+		ap2 temp = cur->actor;
+				while(temp->next!=NULL)
+				{
+					fputs(temp->next->actor,f);
+					if(i++) fputc(',',f);
+			   temp = temp->next;
+				}
+	   fclose(f);
+			deleteNode(Mhead,num,"M");
+			mkList(Mhead);
+		}
 	}
+	
 	else if(!strcmp(option,"d")) {
 		dp cur = gotoNode(num,Dhead,"D");
-		if(cur == NULL) printf("No such record\n");
-		else deleteNode(Dhead,num,"D");
+           if(cur==NULL) {printf("No serialNo.%d's info in the directory\n",num);return;}		
+		else{
+		FILE *f = fopen("directorlog","a");
+	sprintf(str,"delete:%d:%s:%c:%s:",cur->num,cur->director,cur->sex,cur->birth);
+       fputs(str,f);
+		int i=0;
+		tp temp = cur->title;
+				while(temp->next!=NULL)
+				{
+					fputs(temp->next->title,f);
+					if(i++) fputc(',',f);
+			   temp = temp->next;
+			}
+	   fclose(f);
+		 deleteNode(Dhead,num,"D");
+			mkList2(Dhead);
 	}
-	if(!strcmp(option,"a")) {
+	}
+	else if(!strcmp(option,"a")) {
 	    ap cur = gotoNode(num,Ahead,"A");
-		if(cur == NULL) printf("No such record\n");
-		else deleteNode(Ahead,num,"A");
+           if(cur==NULL) {printf("No serialNo.%d's info in the directory\n",num);return;}		
+		else{
+	FILE *f = fopen("actorlog","a");
+	sprintf(str,"delete:%d:%s:%c:%s:",cur->num,cur->actor,cur->sex,cur->birth);
+       fputs(str,f);
+		tp temp = cur->title;
+		int i = 0;
+				while(temp->next!=NULL)
+				{
+					fputs(temp->next->title,f);
+					if(i++) fputc(',',f);
+			   temp = temp->next;
+			}
+
+	   fclose(f);
+		 deleteNode(Ahead,num,"A");
+			mkList3(Ahead);
 	}
+	}
+	else { printf("No option '%s'. Try again.\n\n",option); return;}
 
 }
 
-void Put(char c,void *nodes,char *s){
+int Put(char c,void *nodes,char *s){
 	if(!strcmp(s,"M")){ 
 		mp node = nodes;
 		actors *a = node->actor;
@@ -209,6 +379,7 @@ void Put(char c,void *nodes,char *s){
 		
 		}
 			}
+			else {printf("No option '%c'. Try again. \n",c);return 0;}		
 	}
 	else if(!strcmp(s,"D")) {
 		dp node = nodes;	
@@ -237,6 +408,7 @@ void Put(char c,void *nodes,char *s){
 		a = a->next ; 
 		}
 		}
+			else {printf("No option '%c'. Try again. \n",c);return 0;}		
 	 }
 	else if(!strcmp(s,"A")){
 		ap node = nodes;
@@ -265,30 +437,88 @@ void Put(char c,void *nodes,char *s){
 		a = a->next ; 
 		}	
 		}
+			else {printf("No option '%c'. Try again. \n",c);return 0;}		
 		}
+return 1;
 }
 void update(char *U){
 	char *option = strtok(U," ");
 	char *str = malloc(sizeof(char)*100); 
 	str = strtok(NULL," ");
-	mp node ;
+	int num = atoi(strtok(NULL," "));
 	if(!strcmp(option,"m")){
-		node= gotoNode(atoi(strtok(NULL," ")),Mhead,"M");
+	mp node ;
+		node= gotoNode(num,Mhead,"M");
+           if(node==NULL) {printf("No serialNo.%d's info in the directory\n",num);return;}		
 		for(int i=0;i<strlen(str);i++)
-		{	Put(str[i],node,"M");
-              if(i!=strlen(str)-1) getchar();}
+		{	if(Put(str[i],node,"M"))
+			{ if(i!=strlen(str)-1) getchar();
+			}
+				else{printf("comecome\n"); return;}
+		}
+		FILE *f = fopen("movielog.txt","a");
+	sprintf(str,"update:%d:%s:%s:%s:%d:%d:",node->num,node->title,node->genre,node->director->director,node->year,node->time);
+       fputs(str,f);
+		int i=0;
+		ap2 temp = node->actor;
+				while(temp->next!=NULL)
+				{
+					fputs(temp->next->actor,f);
+					if(i++) fputc(',',f);
+			   temp = temp->next;
+				}
+	   fclose(f);
+	   mkList(Mhead);
 		}
 	if(!strcmp(option,"d")){
-		node= gotoNode(atoi(strtok(NULL," ")),Dhead,"D");
+	dp node ;
+		node= gotoNode(num,Dhead,"D");
+           if(node==NULL) {printf("No serialNo.%d's info in the directory\n",num);return;}		
 		for(int i=0;i<strlen(str);i++)
-		{	Put(str[i],node,"D");
-              if(i!=strlen(str)-1) getchar();}
+		{	if(Put(str[i],node,"D"))
+			{  if(i!=strlen(str)-1) getchar();
+			}
+				else return;
+		}
+		FILE *f = fopen("directorlog.txt","a");
+	sprintf(str,"update:%d:%s:%c:%s:",node->num,node->director,node->sex,node->birth);
+       fputs(str,f);
+		int i=0;
+		tp temp = node->title;
+				while(temp->next!=NULL)
+				{
+					fputs(temp->next->title,f);
+					if(i++) fputc(',',f);
+			   temp = temp->next;
+			}
+				fclose(f);
+				mkList2(Dhead);
 		}
 	if(!strcmp(option,"a")){
-		node= gotoNode(atoi(strtok(NULL," ")),Ahead,"A");
+	ap node ;
+		node= gotoNode(num,Ahead,"A");
+           if(node==NULL) {printf("No serialNo.%d's info in the directory\n",num);return;}		
 		for(int i=0;i<strlen(str);i++)
-		{	Put(str[i],node,"A");
-              if(i!=strlen(str)-1) getchar();}
+		{	if(Put(str[i],node,"A"))
+			{ if(i!=strlen(str)-1) getchar();
+		   
+			}else return;
+		}
+
+	FILE *f = fopen("actorlog.txt","a");
+	sprintf(str,"update:%d:%s:%c:%s:",node->num,node->actor,node->sex,node->birth);
+		printf("%s\n",node->actor);
+       fputs(str,f);
+		tp temp = node->title;
+		int i = 0;
+				while(temp->next!=NULL)
+				{
+					fputs(temp->next->title,f);
+					if(i++) fputc(',',f);
+			   temp = temp->next;
+				}
+				fclose(f);
+				mkList3(Ahead);
 		}
 	return ;
 }
@@ -307,22 +537,26 @@ char *menu(){
 
 
 int main(){
-	FILE *fp,*fp2,*fp3;
+	FILE *ff,*fp,*fp2,*fp3,*f,*f2,*f3;
+	f = fopen("movielist.txt","w");
+	f2 = fopen("directorlist.txt","w");
+	f3 = fopen("actorlist.txt","w");
 	fp = fopen("movielog.txt","r");
 	fp2 = fopen("directorlog.txt","r");
 	fp3 = fopen("actorlog.txt","r");
 	/* 정보 링크드 리스트 생성 */
-
+    curNum1 = curNum2 = curNum3 = 0;
 	init_list(&Mhead);
-	movie_info(&Mhead,fp,fp2);
+	movie_info(&Mhead,fp,f);
+	mkList(Mhead);
 
 	init_list2(&Dhead);
-	director_info(&Dhead,fp2);
-	
+	director_info(&Dhead,fp2,f2);
+    mkList2(Dhead);
+
 	init_list3(&Ahead);
-	actor_info(&Ahead,fp3);
-	
-     
+	actor_info(&Ahead,fp3,f3);
+    mkList3(Ahead); 
 	/* 메뉴 프롬프트 */
     int i;
 	char *Menu = malloc(sizeof(char)*50);
@@ -338,14 +572,14 @@ printf("\n");
 		Menu = menu();
 		Menu2 = strtok(Menu," ");
         Menu3 = strtok(NULL,"\0");
-		if(!strcmp(Menu2,"search"))search(Menu3,fp,fp2,fp3);
+		if(!strcmp(Menu2,"search"))search(Menu3);
 		else if(!strcmp(Menu,"print")) print(Menu3);
 		else if(!strcmp(Menu,"add")) add(Menu3);
 		else if(!strcmp(Menu,"update")) {update(Menu3);getchar();} 
 		else if(!strcmp(Menu,"delete")) delete(Menu3);
 		else if(!strcmp(Menu,"sort")) ;
 		else if(!strcmp(Menu,"save")) ;
-		else if(!strcmp(Menu,"end")){end();  }
+		else if(!strcmp(Menu,"end")){end(); break; }
 		else printf(" 메뉴명을 다시 입력하세요 \n");
 printf("\n");
 	}
